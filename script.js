cbae51478f60a52e6119b63e0fa964b0fbcb0cb7
anastasia-kost <voznikova_a@mail.ru>
2021-12-20 14:17:14 +0300

    completed navigation menu for a smaller screen

---
+++ script.js
@@ -0,0 +1,17 @@
+// toggle menu
+const menuIcon = document.querySelector(".menu-icon");
+const menuItems = document.getElementById("menu-items");
+
+menuItems.style.maxHeight = "0px";
+
+function menuToggle() {
+  if (menuItems.style.maxHeight == "0px") {
+    menuItems.style.maxHeight = "200px";
+  } else {
+    menuItems.style.maxHeight = "0px";
+  }
+}
+
+menuIcon.addEventListener("click", () => {
+  menuToggle();
+});
